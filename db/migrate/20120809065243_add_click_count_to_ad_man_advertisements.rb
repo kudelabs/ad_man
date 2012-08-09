b20a0d1d6f236ab55c2db5ebdcef4f48926f1c83
David Strand <strand1@gmail.com>
2012-08-09 15:02:06 +0800

    modified click_count migration

--- db/migrate/20120809065243_add_click_count_to_ad_man_advertisements.rb
+++ db/migrate/20120809065243_add_click_count_to_ad_man_advertisements.rb
@@ -1,5 +1,5 @@
 class AddClickCountToAdManAdvertisements < ActiveRecord::Migration
   def change
-    add_column :ad_man_advertisements, :click_count, :integer
+    add_column :ad_man_advertisements, :click_count, :integer, :null => false, :default => 0
   end
 end
