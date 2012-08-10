e54d4280bf02280382688dcab9abc79a8fb7941f
Tyler Hu <zhnhu@foxmail.com>
2012-08-10 15:10:07 +0800

    replace the generator name by ad_man:install

---
+++ lib/generators/ad_man/install_generator.rb
@@ -0,0 +1,20 @@
+module AdMan
+	class InstallGenerator < Rails::Generators::Base
+		def create_extended_controller_file
+			create_file "app/controllers/ad_man/advertisements_controller.rb",
+				%Q[require AdMan::Engine.config.root + 'app' + 'controllers' + 'ad_man' + 'advertisements_controller'
+					AdMan::AdvertisementsController.class_eval do 
+					end]
+
+			create_file "app/controllers/ad_man/keywords_controller.rb",
+				%Q[require AdMan::Engine.config.root + 'app' + 'controllers' + 'ad_man' + 'keywords_controller'
+					AdMan::KeywordsController.class_eval do
+					end]
+		end
+		
+		def add_routes
+			route("mount AdMan::Engine, :at => '/ad_man'")
+		end
+	end
+end
+
