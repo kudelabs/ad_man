be4eaae4532493e68d5eadfba4f025a2f7136222
Tyler Hu <zhnhu@foxmail.com>
2012-08-14 12:21:45 +0800

    change some conditions: if request_env['REQUEST_PATH']

--- app/helpers/ad_man/application_helper.rb
+++ app/helpers/ad_man/application_helper.rb
@@ -2,11 +2,11 @@ module AdMan
   module ApplicationHelper
     def link_to_ad(keyword = nil, size = "leaderboard")
   		keyword ||= get_keyword_from_url
-			if !keyword.blank? && !Keyword.find_by_name(keyword).nil?
+			if keyword && !keyword.blank? && !Keyword.find_by_name(keyword).nil?
   		  keyword_id = Keyword.find_by_name(keyword).id
   			ad = Advertisement.render_random_ad(keyword_id)
      	  #grab size? leaderboard or banner
-   	  elsif keyword.blank?
+   	  elsif keyword.nil? || keyword.blank?
    	    ad = Advertisement.render_random_ad
 			end
   		if !ad.nil?
@@ -19,7 +19,7 @@ module AdMan
 
     	#grab the keyword form request url
   	def get_keyword_from_url
-			if !request.env["REQUEST_PATH"].nil?
+			if request.env["REQUEST_PATH"]
   			req_url = request.env["REQUEST_PATH"].split("/")
     		keyword_names = Keyword.all.map{ |keyword| keyword.name }
     		keyword = req_url & keyword_names
