30e68e72336d3f9e0685730ee6b6309c4943ba5b
Matenia Rossides <matenia@gmail.com>
2012-08-15 14:41:20 +1000

    fix all the tests, fix indentation problems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
+require 'rubygems'
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
-
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
@@ -9,11 +9,15 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-# Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
 
 def sample_file(filename = "test.jpeg")
-  File.new("test/fixtures/#{filename}")
+  File.new("#{AdMan::Engine.root}/test/fixtures/#{filename}")
 end
+
+class ActiveSupport::TestCase
+  fixtures :all
+end
+
