460cece015904610482c994ff28c91333b6f86ca
David Strand <strand1@gmail.com>
2012-08-17 17:19:25 +0800

    fixed a few routing problems

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ AdMan::Engine.routes.draw do
   resources :advertisements do
     collection do
       match 'click_through/:id' => "advertisements#click_through", :as => :click_through
-			match '/advertisements/get_ad/:id' => 'advertisements#get_ad'
+			match 'get_ad/:id' => 'advertisements#get_ad', :as => :get_ad
     end
   end
 
