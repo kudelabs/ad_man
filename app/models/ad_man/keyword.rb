5e579e1f1bde154a7b4f1c4d5b3c9ad048bad2c6
TylerHu <zhnhu@foxmail.com>
2012-08-20 17:05:23 +0800

    fixed indentation

--- app/models/ad_man/keyword.rb
+++ app/models/ad_man/keyword.rb
@@ -1,23 +1,24 @@
 module AdMan
   class Keyword < ActiveRecord::Base
     attr_accessible :name
-		has_many :advertisements, :dependent => :destroy
+    has_many :advertisements, :dependent => :destroy
     validates :name, :presence => true, :uniqueness => true	
-  	MAX_COUNT = (AdMan.max_count)?(AdMan.max_count):5
+    MAX_COUNT = (AdMan.max_count)?(AdMan.max_count):5
 
-		def to_s
-			name
-		end
+    def to_s
+      name
+    end
 
-		def number_of_ads
-			self.advertisements.count
-		end
-	  def self.available
-			Keyword.all.select { |keyword| keyword.advertisements.count < MAX_COUNT}
-		end
+    def number_of_ads
+      self.advertisements.count
+    end
+    
+    def self.available
+      Keyword.all.select { |keyword| keyword.advertisements.count < MAX_COUNT}  
+    end
 
-		def self.unavailable 
-			Keyword.all.select { |keyword| keyword.advertisements.count >= MAX_COUNT}
-		end
+    def self.unavailable 
+       Keyword.all.select { |keyword| keyword.advertisements.count >= MAX_COUNT}
+    end
   end
 end
